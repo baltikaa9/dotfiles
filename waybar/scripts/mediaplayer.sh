60e2cbaa702863307c49b209485406c3dad805dc
sergey <sergey@intervolga.ru>
2025-02-04 01:12:22 +0300

    init

---
+++ waybar/scripts/mediaplayer.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+while :
+do
+    player_status=$(playerctl status 2> /dev/null)
+    if [ "$player_status" = "Playing" ]; then
+        echo "$(playerctl metadata title) - $(playerctl metadata artist)"
+    elif [ "$player_status" = "Paused" ]; then
+        echo " $(playerctl metadata title) - $(playerctl metadata artist)"
+    fi
+done
