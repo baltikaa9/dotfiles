8db188b18e7f966821151644e6a053b4976bd55e
sergey <sergey@intervolga.ru>
2025-08-26 21:49:22 +0300

    day and night wallpapers

---
+++ bin/wallpaper.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+dayImg="$HOME/Pictures/wallpapers/Forest-Light.png"
+nightImg="$HOME/Pictures/wallpapers/Forest-Dark.png"
+
+hours=$(date +%H)
+
+if (( hours >= 6 && hours < 18 )); then
+  img="$dayImg"
+else 
+  img="$nightImg"
+fi
+
+
+if command -v swww >/dev/null 2>&1; then
+  # Пример вызова swww (отредактируй опции если нужно)
+  swww img "$img" -o DP-2 &
+  exit $?
+fi
+
+echo "No supported wallpaper tool found (tried: swww)" >&2
+exit 1
