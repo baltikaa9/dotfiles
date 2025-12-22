a2a75920ee78768bd0ba823c2fc2ef33297079ae
sergey <sergey@intervolga.ru>
2025-12-22 22:10:43 +0300

    снег, waybar, обои и еще чето

--- bin/live-wallpaper.sh
+++ bin/live-wallpaper.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
-img="/home/baltika/Pictures/wallpapers/minecraft_sakura.mp4"
+# img="/home/baltika/Pictures/wallpapers/minecraft_sakura.mp4"
+img="/home/baltika/Pictures/wallpapers/girl-blue-eyes.3840x2160.mp4"
 
 mpvpaper -o 'no-audio loop' HDMI-A-1 $img
